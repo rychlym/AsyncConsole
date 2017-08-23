1a2f292f87930d3dac740f53ec21b1fa7b342bec
Mojek Rychly <rychlym@atlas.cz>
2017-08-23 10:02:11 +0200

    async console template (since  .NET 4.5 and till the C# 7.0)

---
+++ AsyncConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AsyncConsole
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // the wrapped main function
+                MainAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.GetType()}! Details: {ex}");
+            }
+#if DEBUG
+            Console.WriteLine("Press any key to end...");
+            Console.ReadKey();
+#endif
+        }
+
+        private static async Task MainAsync()
+        {
+            using (StreamWriter writer = File.CreateText(@"c:\tmp\file.txt"))
+            {
+                await writer.WriteLineAsync("Hello");
+            }
+        }
+    }
+}
